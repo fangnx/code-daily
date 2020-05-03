3a1750091d93dae0a9e6dc29b8fbaf304538785d
fangnx <fangnaxin@gmail.com>
2020-05-03 17:36:02 -0400

    temp

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ClarityModule } from "@clr/angular";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialModule } from "./material/material.module";
+import { QuestionCardComponent } from "./components/question-card/question-card.component";
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, QuestionCardComponent, NavigationBarComponent],
   imports: [
+    ClarityModule,
     BrowserModule,
     AppRoutingModule,
-    MaterialModule,
     NoopAnimationsModule,
   ],
   providers: [],
