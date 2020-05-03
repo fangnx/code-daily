0832a3e977a1e0660790c1ce1058a5c918163f7d
fangnx <fangnaxin@gmail.com>
2020-05-03 16:08:41 -0400

    temp

---
+++ client/src/app/components/question-card/question-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "question-card",
+  templateUrl: "./question-card.component.html",
+  styleUrls: ["./question-card.component.scss"],
+})
+export class QuestionCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
