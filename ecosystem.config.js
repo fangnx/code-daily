c874e33de995e41483894a85aad9f13d365cedb9
fangnx <fangnaxin@gmail.com>
2020-07-07 05:37:09 +0000

    Added ecosystem config for PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    script: './server/node_modules/.bin/ts-node',
+    watch: './server/src/main.ts' 
+  }],
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
